314b577984bf580eceeba077b3094c7a13896351
Haniefa Idalene <hidalene@student.wethinkcode.co.za>
2025-04-19 17:18:50 +0300

    Initial Commit

---
+++ L1demo/L1demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1demoApp.swift
+//  L1demo
+//
+//  Created by Haniefa Binsaad on 2025/04/19.
+//
+
+import SwiftUI
+
+@main
+struct L1demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ L1demoTests/L1demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  L1demoTests.swift
+//  L1demoTests
+//
+//  Created by Haniefa Binsaad on 2025/04/19.
+//
+
+import Testing
+@testable import L1demo
+
+struct L1demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
